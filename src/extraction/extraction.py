305448bcaba1c43f5dd6127abbbf676f68d66e40
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-03-28 03:53:29 +0100

    Fonction d'extraction de données d'un .csv et tests

--- src/extraction/extraction.py
+++ src/extraction/extraction.py
@@ -8,16 +8,31 @@ params:
 return:
     - une liste de dictionnaire
 """
-def read_json(filePath):
+def extractFromJson(filePath):
     try:
         with open(filePath, 'r') as json_file:
             data = json.load(json_file)
         df = pd.json_normalize(data) # traitement des données imbriquées
         return df.to_dict(orient='records')
     except Exception as e:
-        print("Erreur lors de la lecture du fichier")
+        print(f"Erreur lors de la lecture du fichier : {e}")
+        return None
+
+"""
+fonction d'extraction de données d'un fichier csv
+params:
+    - filePath: chemin vers le ficher
+return:
+    - une liste de dictionnaire
+"""
+def extractFromCSV(filePath):
+    try:
+        df = pd.read_csv(filePath)
+        return df.to_dict(orient='records')
+    except Exception as e:
+        print(f"Erreur lors de la lecture du fichier : {e}")
         return None
     
 #test
-donnee = read_json("../../dataset/school.json")
-print(pd.DataFrame(donnee))
+donnees = extractFromJson("../../dataset/school.json")
+print(pd.DataFrame(donnees))
