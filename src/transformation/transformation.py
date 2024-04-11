7bdd4fcc257f37d5a23d8f0b86d6ff76ab938b4f
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-04-12 01:16:26 +0200

    Prise en compte de colonnes multiples et des critères multiples dans les fonctions de filtre sur chaine de caractères

--- src/transformation/transformation.py
+++ src/transformation/transformation.py
@@ -18,32 +18,42 @@ def cleanColumnDupilcates(data: List):
     return dfToDict(cleanDf)
 
 # valable pour les string et number
-def equalsTo(data: List, column, constValue):
+def equalsTo(data: List, columns, constValues):
     df = pd.DataFrame(data)
-    result = df[column].isin(constValue)
+    result = True
+    for elmt in columns:
+        result = result & df[elmt].isin(constValues)
     return dfToDict(df[result])
 
 # valable pour les string et number
-def notEqualsTo(data: List, column, constValue):
+def notEqualsTo(data: List, columns, constValues):
     df = pd.DataFrame(data)
-    result = df[column].isin(constValue)
+    result = True
+    for elmt in columns:
+        result = result & df[elmt].isin(constValues)
     return dfToDict(df[~result])
 
-def strStartWith(data: List, column, constValue):
+def strStartWith(data: List, columns, constValues):
     df = pd.DataFrame(data)
-    result = df[column].str.startswith(tuple(constValue))
+    result = True
+    for elmt in columns:
+        result = result & df[elmt].str.startswith(tuple(constValues))
     return dfToDict(df[result])
 
-def strEndWith(data: List, column, constValue):
+def strEndWith(data: List, columns, constValues):
     df = pd.DataFrame(data)
-    result = df[column].str.endswith(tuple(constValue))
+    result = True
+    for elmt in columns:
+        result = result & df[elmt].str.endswith(tuple(constValues))
     return dfToDict(df[result])
 
-def strContains(data: List, column, constValue):
+def strContains(data: List, columns, constValues):
     df = pd.DataFrame(data)
-    result = df[column].str.contains('|'.join(constValue))
+    result = True
+    for elmt in columns:
+        result = result & df[elmt].str.contains('|'.join(constValues))
     return dfToDict(df[result])
 
 data = cleanColumnDupilcates(extractFromCSV("../../dataset/students.csv"))
-result = strContains(data, "Nom", ["Mar", "Du"])
+result = strContains(data, ["Nom", "Prénom"], ["d", "a"])
 print(pd.DataFrame(result))
