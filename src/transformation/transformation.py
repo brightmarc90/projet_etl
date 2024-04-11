9e945e026a1989cf01b660dfc740775acb7d07d3
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-04-11 18:52:45 +0200

    fonctions de filtre sur critères pour les chaines de caractères

--- src/transformation/transformation.py
+++ src/transformation/transformation.py
@@ -2,18 +2,48 @@ from typing import List
 import pandas as pd
 import sys
 sys.path.append('../extraction')
-from extraction import *
+from extraction import extractFromCSV
 
+def dfToDict(data: pd.DataFrame):
+    return data.to_dict(orient="records")
 
 def cleanLineDupilcates(data: List):
     df = pd.DataFrame(data)
     cleanDf = df.drop_duplicates()
-    return cleanDf.to_dict(orient="records")
+    return dfToDict(cleanDf)
 
 def cleanColumnDupilcates(data: List):
     df = pd.DataFrame(data)
     cleanDf = df.transpose().drop_duplicates().transpose()
-    return cleanDf.to_dict(orient="records")
+    return dfToDict(cleanDf)
+
+# valable pour les string et number
+def equalsTo(data: List, column, constValue):
+    df = pd.DataFrame(data)
+    result = df[column].isin(constValue)
+    return dfToDict(df[result])
+
+# valable pour les string et number
+def notEqualsTo(data: List, column, constValue):
+    df = pd.DataFrame(data)
+    result = df[column].isin(constValue)
+    return dfToDict(df[~result])
+
+def strStartWith(data: List, column, constValue):
+    df = pd.DataFrame(data)
+    result = df[column].str.startswith(tuple(constValue))
+    return dfToDict(df[result])
+
+def strEndWith(data: List, column, constValue):
+    df = pd.DataFrame(data)
+    result = df[column].str.endswith(tuple(constValue))
+    return dfToDict(df[result])
+
+def strContains(data: List, column, constValue):
+    df = pd.DataFrame(data)
+    result = df[column].str.contains('|'.join(constValue))
+    return dfToDict(df[result])
 
 data = cleanColumnDupilcates(extractFromCSV("../../dataset/students.csv"))
-print(pd.DataFrame(data))
+result = strContains(data, "Nom", ["Mar", "Du"])
+print(pd.DataFrame(result))
