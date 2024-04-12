f92ab3383079aa528197db78d85271330bd53ee6
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-04-12 04:15:02 +0200

    Fonctions de filtre de comparaison sur les numériques

--- src/transformation/transformation.py
+++ src/transformation/transformation.py
@@ -17,7 +17,7 @@ def cleanColumnDupilcates(data: List):
     cleanDf = df.transpose().drop_duplicates().transpose()
     return dfToDict(cleanDf)
 
-# valable pour les string et number
+# valable pour les string
 def equalsTo(data: List, columns, constValues):
     df = pd.DataFrame(data)
     result = True
@@ -25,7 +25,6 @@ def equalsTo(data: List, columns, constValues):
         result = result & df[elmt].isin(constValues)
     return dfToDict(df[result])
 
-# valable pour les string et number
 def notEqualsTo(data: List, columns, constValues):
     df = pd.DataFrame(data)
     result = True
@@ -54,6 +53,73 @@ def strContains(data: List, columns, constValues):
         result = result & df[elmt].str.contains('|'.join(constValues))
     return dfToDict(df[result])
 
+# pour les numériques
+def isNumeric(valeur):
+    try:
+        float(valeur)
+        return True
+    except ValueError:
+        return False
+
+def eq(df, columns, constValue, result):
+    r = result
+    for elmt in columns:
+        r = r & (df[elmt] == constValue)
+    return dfToDict(df[r])
+
+def neq(df, columns, constValue, result):
+    r = result
+    for elmt in columns:
+        r = r & (df[elmt] != constValue)
+    return dfToDict(df[r])
+
+def gt(df, columns, constValue, result):
+    r = result
+    for elmt in columns:
+        r = r & (df[elmt] > constValue)
+    return dfToDict(df[r])
+
+def gte(df, columns, constValue, result):
+    r = result
+    for elmt in columns:
+        r = r & (df[elmt] >= constValue)
+    return dfToDict(df[r])
+
+def lt(df, columns, constValue, result):
+    r = result
+    for elmt in columns:
+        r = r & (df[elmt] < constValue)
+    return dfToDict(df[r])
+
+def lte(df, columns, constValue, result):
+    r = result
+    for elmt in columns:
+        r = r & (df[elmt] <= constValue)
+    return dfToDict(df[r])
+
+def switch_case(df, columns, constValue, sign, result):
+    switcher = {
+        "eq": eq,
+        "neq": neq,
+        "gt": gt,
+        "gte": gte,
+        "lt": lt,
+        "lte": lte
+    }
+    # récup de la fonction via le switcher
+    func = switcher.get(sign, lambda: "Cas invalide")
+    # Exécution de la fion
+    return func(df, columns, constValue, result)
+
+def nbCompare(data: List, columns, constValue, sign):
+    if isNumeric(constValue):
+        df = pd.DataFrame(data)
+        result = True
+        return switch_case(df, columns, constValue, sign, result)
+    else:
+        print("Merci de préciser une valeur de comparaison numérique")
+        return None
+    
 data = cleanColumnDupilcates(extractFromCSV("../../dataset/students.csv"))
-result = strContains(data, ["Nom", "Prénom"], ["d", "a"])
+result = nbCompare(data, ["Physique", "Français"], 13, "neq")
 print(pd.DataFrame(result))
