0ed4515eb1263d162d8d0ffa304a8f8e5b99f474
brightmarc90 <120368002+brightmarc90@users.noreply.github.com>
2024-04-16 00:56:17 +0200

    Configuration du pipeline des élèves et exécution via le main

--- src/loading/loading.py
+++ src/loading/loading.py
@@ -2,7 +2,6 @@ from typing import List
 import pandas as pd
 import sys
 sys.path.append('../extraction')
-from extraction import extractFromCSV
 import os
 
 def loadInJsonOrXML(data: List, destPath, fileName, fileType):
@@ -10,16 +9,9 @@ def loadInJsonOrXML(data: List, destPath, fileName, fileType):
     # Vérifier si le dossier de destination existe, sinon le créer
     if not os.path.exists(destPath):
         os.makedirs(destPath)
-    if(fileType == "json"):
+    if(fileType == "JSON"):
         pathToFile = os.path.join(destPath, fileName+".json")
         df.to_json(pathToFile, orient="records", indent=4)
     else:
         pathToFile = os.path.join(destPath, fileName+".xml")
-        df.to_xml(pathToFile, root_name="students", row_name="student")
-
-
-data = extractFromCSV("../../dataset/students.csv")
-
-print(pd.DataFrame(data))
-
-loadInJsonOrXML(data, "../../laoded_files", "data", "xml")
+        df.to_xml(pathToFile, root_name="data", row_name="element")
--- src/transformation/transformation.py
+++ src/transformation/transformation.py
@@ -10,18 +10,3 @@ from cleaners.cleaners import *
 
 data = cleanColumnDupilcates(extractFromCSV("../../dataset/students.csv"))
 print(data)
-"""#result1 = addColumn(data, ["Min", "Max", "Moyenne"])
-#result2 = fillColumnByOps(result1, "Min", "min", ["Français", "Histoire"])
-result2 = inferColumnType(data, ['Date de naissance'], "date")
-result2 = cleanDateOutliers(result2)
-result2 = inferColumnType(result2, ['Mathématiques', 'Français', 'Histoire'], "num")
-result2 = correctNumOutliers(result2, ['Mathématiques', 'Français', 'Histoire'], "med")
-result2 = normaliseCateg(result2, "Sexe", [("Homme", "H"), ("Femme", "F")])
-print(pd.DataFrame(result2)) """
-"""
-faire 4 fonctions demain
-    - inférence de type avec au moins 2 cas (str, date) => done
-    - finaliser la fonction cleanDateOutliers => done
-    - fonction de correction de valeurs numériques aberrantes (avec liste de colonnes en param) => done
-    - fonction de normalisation de données ex: Homme Femme en H F
-"""
